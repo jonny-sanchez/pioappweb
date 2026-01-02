474fa9db0bebe27f008327ab12637f8d55708ca7
pinulito\oscar.alfaro <oscar.alfaro@corporacionalisa.com>
2026-01-02 15:23:16 -0600

    Ajustes de docker y de visitas

--- app/src/types/Visita.ts
+++ app/src/types/Visita.ts
@@ -1,13 +1,14 @@
 export interface Visita {
   codigo_empresa: string;
   codigo_tienda: string;
-  nombre_tienda: string;
+  tienda_nombre: string;
   direccion_tienda: string;
   tipo_visita: string;
   fecha_hora_visita: string;
   codigo_usuario_visita: string;
-  nombre_usuario_visita: string;
   gps_longitud_visita: number;
   gps_latitud_visita: number;
-  comentario_visita: string;
+  comentario: string;
+  createdAt: string;
+  url_image: string
 }
